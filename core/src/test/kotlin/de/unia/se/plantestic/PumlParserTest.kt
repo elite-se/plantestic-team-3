da8b8bd2cddd95a8aeac9af97733b0fa4be97786
Tobias Schmidt <tobias.schmidt@in.tum.de>
2020-06-28 13:18:46 +0200

    fix test

--- core/src/test/kotlin/de/unia/se/plantestic/PumlParserTest.kt
+++ core/src/test/kotlin/de/unia/se/plantestic/PumlParserTest.kt
@@ -8,20 +8,17 @@ import org.eclipse.emf.ecore.EObject
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl
 import plantuml.puml.Activate
 import plantuml.puml.Alternative
-import plantuml.puml.SequenceUml
-import plantuml.puml.UseLeft
 import plantuml.puml.Participant
+import plantuml.puml.UseLeft
 
 class PumlParserTest : StringSpec({
 
     "Parsing works for the minimal example" {
         MetaModelSetup.doSetup()
 
-        val umlDiagram = PumlParser.parse(MINIMAL_HELLO_INPUT_PATH)
-        printModel(umlDiagram)
+        val sequenceDiagram = PumlParser.parse(MINIMAL_HELLO_INPUT_PATH)
+        printModel(sequenceDiagram)
 
-        (umlDiagram.umlDiagrams[0] is SequenceUml) shouldBe true
-        val sequenceDiagram = umlDiagram.umlDiagrams[0] as SequenceUml
         sequenceDiagram.umlElements.size shouldBe 4
         (sequenceDiagram.umlElements[0] as Participant).name shouldBe "A"
         (sequenceDiagram.umlElements[1] as Participant).name shouldBe "B"
@@ -36,11 +33,9 @@ class PumlParserTest : StringSpec({
     "Parsing works for the complex hello example" {
         MetaModelSetup.doSetup()
 
-        val umlDiagram = PumlParser.parse(COMPLEX_HELLO_INPUT_PATH)
-        printModel(umlDiagram)
+        val sequenceDiagram = PumlParser.parse(COMPLEX_HELLO_INPUT_PATH)
+        printModel(sequenceDiagram)
 
-        (umlDiagram.umlDiagrams[0] is SequenceUml) shouldBe true
-        val sequenceDiagram = umlDiagram.umlDiagrams[0] as SequenceUml
         sequenceDiagram.umlElements.size shouldBe 3
         (sequenceDiagram.umlElements[0] as Participant).name shouldBe "A"
         (sequenceDiagram.umlElements[1] as Participant).name shouldBe "B"
@@ -59,11 +54,9 @@ class PumlParserTest : StringSpec({
     "Parsing works for the rerouting example" {
         MetaModelSetup.doSetup()
 
-        val umlDiagram = PumlParser.parse(REROUTE_INPUT_PATH)
-        printModel(umlDiagram)
+        val sequenceDiagram = PumlParser.parse(REROUTE_INPUT_PATH)
+        printModel(sequenceDiagram)
 
-        (umlDiagram.umlDiagrams[0] is SequenceUml) shouldBe true
-        val sequenceDiagram = umlDiagram.umlDiagrams[0] as SequenceUml
         sequenceDiagram.umlElements.filterIsInstance<Participant>().size shouldBe 3
         val alternative = ((sequenceDiagram.umlElements[5] as Activate)
             .umlElements[2] as Activate).umlElements[0] as Alternative
@@ -75,11 +68,9 @@ class PumlParserTest : StringSpec({
     "Parsing works for the xcall example" {
         MetaModelSetup.doSetup()
 
-        val umlDiagram = PumlParser.parse(XCALL_INPUT_PATH)
-        printModel(umlDiagram)
+        val sequenceDiagram = PumlParser.parse(XCALL_INPUT_PATH)
+        printModel(sequenceDiagram)
 
-        (umlDiagram.umlDiagrams[0] is SequenceUml) shouldBe true
-        val sequenceDiagram = umlDiagram.umlDiagrams[0] as SequenceUml
         sequenceDiagram.umlElements.filterIsInstance<Participant>().size shouldBe 7
         val alternative = (sequenceDiagram.umlElements[7] as Activate).umlElements[4] as Alternative
 
